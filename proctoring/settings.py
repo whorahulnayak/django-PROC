01a67fe736d4885efede8b3d981dfc9a272bcbc7
sourabh <Hiremathsourabh12@gmail.com>
2022-02-23 12:40:29 +0530

    secured

--- proctoring/settings.py
+++ proctoring/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5y9-=f69%a3z4gly%m8gr^=o^-rrctwq=i=y=p8xa#fa=lrh9@'
+SECRET_KEY = '87==$(#G`U6GEG&>?]*6`[E0XKPP3[_}=+15\:&1}M,XA9PR=V'
+#87==$(#G`U6GEG&>?]*6`[E0XKPP3[_}=+15\:&1}M,XA9PR=V
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -47,7 +48,6 @@ INSTALLED_APPS = [
 
 ]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -168,3 +168,15 @@ EMAIL_HOST_PASSWORD = 'lleewcjqparpojmf'
 #zbavrklbumsocils
 
 django_heroku.settings(locals())
+
+CSRF_COOKIE_SECURE = True #to avoid transmitting the CSRF cookie over HTTP accidentally.
+SESSION_COOKIE_SECURE = True #to avoid transmitting the session cookie over HTTP accidentally.
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 86400  # 1 day
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
