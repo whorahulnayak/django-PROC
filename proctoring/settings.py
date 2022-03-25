567c5fdf05d179b8707416afc537d729c2dc3677
Rahul Anil Nayak <whorahulnayak@gmail.com>
2022-03-26 00:07:29 +0530

    initial check commit

--- proctoring/settings.py
+++ proctoring/settings.py
@@ -20,7 +20,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'js', 'serviceworker.js')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -43,9 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'Proctor.apps.ProctorConfig',
-
+    'pwa',
 ]
 
 
--- proctoring/urls.py
+++ proctoring/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Proctor.urls')),
+    path('', include("pwa.urls")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
